5bcb1cfe5f4ed2622d02232a6d1d8a362caaac23
Tomer Cohen <you@example.com>
2025-12-09 20:22:10 +0200

    connect to sql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route,  } from "react-router-dom";
-import Login from './components/Login'
-import Register from './components/Register';
-import HomePage from './components/HomePage';
-import ForgotPassword from './components/ForgotPassword';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// שינוי קריטי: הוספת שם התיקייה + שם הקובץ + הסיומת .jsx
+import Login from './components/Login/Login.jsx'; 
+import Register from './components/Register/Register.jsx';
+import HomePage from './components/HomePage/HomePage.jsx';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword.jsx';
 
 const App = () => {
-
   return (
     <>
       <Router>
